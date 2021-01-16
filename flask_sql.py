d7091c7f1ac271633adc7681f3aa02ac726c2f3f
W1nson <40487548+W1nson@users.noreply.github.com>
2021-01-17 05:29:41 +0800

    Works with the get function of the restfulAPI

--- flask_sql.py
+++ flask_sql.py
@@ -10,8 +10,7 @@ workout_count = 50
 yoga_count = 50 
 
 
-conn = psycopg2.connect(database="YoutubeVid", user="postgres", password="1229", host="127.0.0.1", port="5432")
-cur = conn.cursor()
+
 
 app = Flask(__name__)
 api = Api(app)
@@ -19,28 +18,43 @@ api = Api(app)
 
 class med(Resource):
     def get(self): 
-      num = randint(1, med_count)
-      query = "select * from Meditation where ID = %s"
-      cur.execute(query, num)
-      vid = cur.fetchone()
-      return {"videoID" : vid[1]}, 200
+        conn = psycopg2.connect(database="YoutubeVid", user="postgres", password="1229", host="127.0.0.1", port="5432")
+        cur = conn.cursor()
+        num = randint(1, med_count)
+        
+        query = "select * from Meditation where id = %s"
+        cur.execute(query, (num,))
+        vid = cur.fetchone()
+        cur.close()
+        conn.close()
+        return {"videoID" : vid[1]}, 200
 
 
 class yoga(Resource):
     def get(self): 
-      num = randint(1, yoga_count)
-      query = "select * from Yoga where ID = %s"
-      cur.execute(query, num)
-      vid = cur.fetchone()
-      return {"videoID" : vid[1]}, 200
+        conn = psycopg2.connect(database="YoutubeVid", user="postgres", password="1229", host="127.0.0.1", port="5432")
+        cur = conn.cursor()
+        num = randint(1, yoga_count)
+      
+        query = "select * from Yoga where id = %s"
+        cur.execute(query, (num,))
+        vid = cur.fetchone()
+        cur.close()
+        conn.close()
+        return {"videoID" : vid[1]}, 200
 
 class workout(Resource):
     def get(self): 
-      num = randint(1, workout_count)
-      query = "select * from Workout where ID = %s"
-      cur.execute(query, num)
-      vid = cur.fetchone()
-      return {"videoID" : vid[1]}, 200
+        conn = psycopg2.connect(database="YoutubeVid", user="postgres", password="1229", host="127.0.0.1", port="5432")
+        cur = conn.cursor()
+        num = randint(1, workout_count)
+      
+        query = "select * from Workout where id = %s"
+        cur.execute(query, (num,))
+        vid = cur.fetchone()
+        cur.close()
+        conn.close()
+        return {"videoID" : vid[1]}, 200
 
 api.add_resource(med, "/api/med")
 api.add_resource(yoga, "/api/yoga")
